32af96ea855dae3f23bd75acf66951e15ad2f166
3Alan <1540703192@qq.com>
2021-08-18 22:55:53 +0800

    8-18

---
+++ src/components/button/index.tsx
@@ -0,0 +1,6 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
+
+type NativeButtonProps = ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = AnchorHTMLAttributes<HTMLElement>;
+
+// ts Partial
