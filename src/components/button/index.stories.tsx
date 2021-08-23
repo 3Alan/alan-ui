98fda84c26be2b50f2f9311e08500fc1d68bd28a
3Alan <1540703192@qq.com>
2021-08-23 17:37:17 +0800

    complete build work

---
+++ src/components/button/index.stories.tsx
@@ -0,0 +1,8 @@
+import Button from './index';
+
+export default {
+  title: 'Components/Button',
+  component: Button
+};
+
+export const Default = () => <Button>Button</Button>;
--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './stories/Button';
-export { default as Header } from './stories/Header';
-export { default as Page } from './stories/Page';
