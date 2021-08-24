baca33ca9d3a9d15118bbc12e3da0c71f0efa484
3Alan <1540703192@qq.com>
2021-08-24 18:18:10 +0800

    test

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,44 @@
 import { AnchorHTMLAttributes, ButtonHTMLAttributes, FC } from 'react';
+import classNames from 'classnames';
 
 type NativeButtonProps = ButtonHTMLAttributes<HTMLElement>;
 type AnchorButtonProps = AnchorHTMLAttributes<HTMLElement>;
 
+export interface ButtonProps {
+  size?: 'small' | 'default';
+  type?: 'standard' | 'primary';
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}
+
+const cls = 'btn';
+
 // ts Partial
-const Button: FC = ({ children }) => <button>{children}</button>;
+const Button: FC<
+  ButtonProps & Omit<React.ButtonHTMLAttributes<any>, 'onClick' | 'type'>
+> = props => {
+  const { children, type, className, size, ...restProps } = props;
+
+  const classes = classNames(
+    cls,
+    `${cls}-base`,
+    {
+      [`${cls}-${type}`]: type,
+      [`${cls}-${size}`]: size !== 'default'
+    },
+    className
+  );
+
+  return (
+    <button className={classes} {...restProps}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'primary',
+  size: 'default'
+};
 
 export default Button;
