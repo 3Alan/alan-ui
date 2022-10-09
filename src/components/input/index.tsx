99805046f4daed0d6975873e43b8d3a557316be7
3Alan <1540703192@qq.com>
2022-10-09 18:18:53 +0800

    feat: new ui style

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,36 +1,38 @@
 import classNames from 'classnames';
-import { FC, FocusEvent, InputHTMLAttributes, useState } from 'react';
+import { FC, FocusEvent, InputHTMLAttributes } from 'react';
 import { RoughWrap } from '../roughWrap';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
   className?: string;
 }
 
 const cls = 'alan-input';
 
 export const Input: FC<InputProps> = (props) => {
-  const { className, onFocus, ...restProps } = props;
-  const [roughProps, setRoughProps] = useState({
-    stroke: '#d9d9d9'
-  });
+  const { className, label, onFocus, ...restProps } = props;
 
   const onInternalFocus = (e: FocusEvent<HTMLInputElement>) => {
-    setRoughProps({
-      stroke: '#000000d9'
-    });
     onFocus?.(e);
   };
 
   const onInternalBlur = (e: FocusEvent<HTMLInputElement>) => {
-    setRoughProps({
-      stroke: '#d9d9d9'
-    });
     onFocus?.(e);
   };
 
   return (
-    <RoughWrap shape="rectTangle" roughProps={roughProps} customElement="div" className={classNames(cls, className)}>
-      <input data-testid="input" {...restProps} onFocus={onInternalFocus} onBlur={onInternalBlur} />
+    <RoughWrap
+      shape="roundedRectTangle"
+      radius="9 9 9 9"
+      contentClassName={`${cls}-wrap`}
+      roughProps={{ roughness: 0, stroke: '#1F2937', fillStyle: 'solid' }}
+      customElement="div"
+      className={classNames(cls, className)}
+    >
+      <div className={`${cls}-content-wrap`}>
+        <span className={`${cls}-label`}>{label}</span>
+        <input data-testid="input" {...restProps} onFocus={onInternalFocus} onBlur={onInternalBlur} />
+      </div>
     </RoughWrap>
   );
 };
